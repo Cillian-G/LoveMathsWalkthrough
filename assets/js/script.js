0260c7b4ca32ac02356712d549ae7bac010ce6d6
Cillian G <cilliangoan221@gmail.com>
2021-12-08 12:16:45 +0000

    Add focus to answer-box, add Enter key functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,25 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         })
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition")
+
 })
 /**
  * The Main game "loop", called when the script is first laoded
  *  and after teh users answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
